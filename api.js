18340fed0f0531e35f52f2080b73b85c5d729097
covidfollower <66930837+covidfollower@users.noreply.github.com>
2020-06-14 23:47:51 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,282 @@
+const option = {
+  method: "GET",
+  mode: "cors",
+  cache: "default",
+};
+function callFetch(country) {
+  console.log(country)
+  fetch(
+    `https://api.covid19api.com/total/dayone/country/${country}`,
+    option
+  ).then((response) => {
+    response.json().then((data) => apiConstruction(data));
+  });
+}
+
+// https://api.covid19api.com/dayone/country/china
+
+// let buttonTest = document.getElementById("test");
+// buttonTest.addEventListener("click", function () {
+
+// });
+
+let label = [];
+let contador = 0;
+
+for (let index = 0; index < 150; index += 1) {
+  contador += 1;
+  label.push(contador);
+}
+let dataSetsInfo = {
+  datasets: [],
+};
+function apiConstruction(data) {
+  let dayDeaths = [];
+
+  for (let index = 0; index < data.length; index += 1) {
+    dayDeaths.push(data[index].Deaths);
+  }
+
+  function randomColor() {
+    let r = Math.floor(Math.random() * 205 + 50);
+    let g = Math.floor(Math.random() * 205 + 50);
+    let b = Math.floor(Math.random() * 205 + 50);
+
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+  
+  function createChartData(data) {
+    let newLineColor = randomColor();
+    let newData = {
+      data: [],
+      backgroundColor: newLineColor,
+      borderColor: newLineColor,
+      borderWidth: 3,
+      pointRadius: 0,
+      label: data[1].Country,
+      fill: false,
+    };
+    for (let index = 0; index < data.length; index += 1) {
+      newData.data.push(data[index].Deaths);
+    }
+
+    let oldData = myChart.data.datasets[0];
+    dataSetsInfo.datasets.push(newData);
+    myChart.data.dataset = dataSetsInfo.dataset;
+  }
+
+  addData(myChart, createChartData(data));
+
+  function addData(chart, data) {
+    Object.assign(chart.data.datasets, data);
+    chart.update();
+  }
+}
+
+let ctx = document.getElementById("myChart2").getContext("2d");
+let myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: label,
+    datasets: dataSetsInfo.datasets,
+  },
+  options: {
+    tooltips: {
+      mode: "nearest",
+      intersect: false,
+    },
+    hover: {
+      mode: "nearest",
+      intersect: false,
+    },
+    scales: {
+      yAxes: [
+        {
+          type: "linear",
+          scaleLabel: {
+            display: true,
+            labelString: "Total Deaths",
+            fontSize: 18,
+          },
+          ticks: {
+            display: true,
+            fontSize: 15,
+            fontColor: "#eee",
+            min: 0,
+            suggestedMax: 30000,
+            stepSize: 1000,
+            maxTicksLimit: 30,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Days from day one",
+            fontSize: 18,
+          },
+          ticks: {
+            display: true,
+            fontSize: 15,
+            fontColor: "#eee",
+            maxRotation: 0,
+          },
+        },
+      ],
+    },
+    responsive: true,
+    legend: {
+      labels: {
+        fontColor: "#eee",
+        fontSize: 15,
+      },
+    },
+    title: {
+      display: true,
+      text: "TOTAL DEATHS FROM DAY ONE",
+      fontSize: 18,
+      fontColor: "#eee",
+    },
+  },
+});
+
+callFetch("brazil");
+callFetch("france");
+callFetch("united-states");
+callFetch("italy");
+callFetch("spain");
+let arr = [];
+let arrCriado = [
+  "brazil",
+  "world",
+  "spain",
+  "italy",
+  "france",
+  "united-states",
+];
+
+/* let generateGraphic = document.getElementsByClassName("countries")[0];
+generateGraphic.addEventListener("change", graphicUpdate);
+*/
+const noData = [
+        'anguilla', 
+        'aruba',
+        'australia',
+        'bermuda', 
+        'british-virgin-islands', 
+        'caribbean-netherlands',
+        'cayman-islands',
+        'channel-islands',
+        'china',
+        'curaçao',
+        'diamond-princess', 
+        'faeroe-islands',
+        'falkland-islands',
+        'french-guiana',
+        'french-polyanesia',
+        'gibraltar',
+        'greenland',
+        'guadeloupe',
+        'hong kong',
+        'isle of man',
+        'martinique',  
+        'macao',
+        'ms-zaandam',
+        'reunion',
+        'montserrat',
+        'mayotte',
+        'turks-and-caicos'
+      ]
+let noDeathCountries = [];
+function graphicUpdate() {
+  let trInfo = document.querySelectorAll("tr");
+  for (i = 1; i < trInfo.length; i += 1) {
+    if(noData.includes(trInfo[i].classList.item(1).toLowerCase())){
+
+    } else {
+      arr.push(trInfo[i].classList.item(1).toLowerCase());
+    }
+  }
+
+  let nameChange = [
+    'car', 'central-african-republic',
+    'congo','democratic-republic-of-the-congo',
+    'drc','democratic-republic-of-the-congo',
+    'iran, Islamic Republic of','iran',
+    'timor-leste','east-timor',
+    'usa', 'united-states'
+  ]
+
+  for (let i = 0; i < arr.length; i += 1) {
+    for ( let j = 0 ; j < nameChange.length ; j += 2) {
+      if (arr[i] === nameChange[j]) { 
+        arr[i] = nameChange[(j+1)];
+      }
+    }
+    
+    
+    if (arr[i] !== "world") {
+      if (arrCriado.includes(arr[i]) === false) {
+        arrCriado.push(arr[i]);
+        callFetch(arr[i]);
+        
+      }
+    }
+  }
+}
+
+
+let chartChange = document.getElementById("myChart2");
+
+let removement = document.getElementsByClassName("remove")[0];
+
+removement.addEventListener("click", removingFromGraph);
+
+function removingFromGraph() {
+  let trSelected = document.getElementsByTagName("tr");
+  let countryToBeRemoved = "";
+  for (let i = 0; i < trSelected.length; i += 1) {
+    if (trSelected[i].classList.contains("bg-warning") === true) {
+      countryToBeRemoved = trSelected[i].classList.item(1).toLowerCase();
+    }
+  }
+  let toRemoveDatasetIndex;
+
+  if (countryToBeRemoved !== "" && countryToBeRemoved !== "world") {
+    let arrayToSearch = myChart.data.datasets;
+
+    for (i = 0; i < arrayToSearch.length; i += 1) {
+      if (arrayToSearch[i].label.toLowerCase() === countryToBeRemoved) {
+        toRemoveDatasetIndex = i;
+        arrCriado.splice(arrCriado.indexOf(countryToBeRemoved), 1);
+        countryToBeRemoved = "";
+      }
+    }
+  }
+  if (toRemoveDatasetIndex !== undefined) {
+    removeData(myChart, toRemoveDatasetIndex);
+    toRemoveDatasetIndex = undefined;
+    removingFromGraph();
+  }
+}
+
+function removeData(chart, removedDatasetIndex) {
+  chart.data.datasets.splice(removedDatasetIndex, 1);
+  chart.update();
+}
+
+function dataNumberValidity() {
+  let trInfo = document.querySelectorAll("tr");
+  for (i = 1; i < trInfo.length; i += 1) {
+    let rowClassPIcker = trInfo[i].classList.item(0);
+    let newNdFinder = document.getElementsByClassName(rowClassPIcker);
+    if (newNdFinder[4].innerHTML == 'N/D') {
+      if (trInfo.classList === undefined) {
+        noDeathCountries.push(newNdFinder[1].innerHTML);
+        break;
+      }
+    }
+  }
+  return noDeathCountries;
+}
---
+++ script.js
@@ -0,0 +1,208 @@
+const spanCases = document.getElementsByClassName("span-cases");
+const spanCritical = document.getElementsByClassName("span-critical");
+const spanDeaths = document.getElementsByClassName("span-deaths");
+const spanTests = document.getElementsByClassName("span-tests");
+const spanRecovered = document.getElementsByClassName("span-recovered");
+const spanCasesMillion = document.getElementsByClassName("span-cases-million");
+const spanDeathsMillion = document.getElementsByClassName(
+  "span-deaths-million"
+);
+const spanTestsMillion = document.getElementsByClassName("span-tests-million");
+const spanTodayCases = document.getElementsByClassName("span-today-cases");
+const spanTodayDeaths = document.getElementsByClassName("span-today-deaths");
+const totalCases = document.getElementById("total-cases");
+const options = {
+  method: "GET",
+  mode: "cors",
+  cache: "default",
+};
+
+fetch("https://coronavirus-19-api.herokuapp.com/countries", options).then(
+  (response) => {
+    response.json().then((data) => mainConstruction(data));
+  }
+);
+
+const selectCountry = document.getElementById("countries");
+if (selectCountry.className === "default") {
+  selectCountry.style.color = "lightgray";
+}
+const tableHead = document.getElementById("tbHead");
+
+function mainConstruction(data) {
+  /* CONSTRUÇÃO DO OPTIONS DO SELECT */
+  function optionConstruction(data) {
+    let arr = [];
+    for (let i = 0; i < data.length; i += 1) {
+      arr.push(data[i].country);
+    }
+    arr.sort();
+    for (let i = 0; i < arr.length; i += 1) {
+      let options = document.createElement("option");
+      options.value = arr[i];
+      options.innerHTML = arr[i];
+      selectCountry.appendChild(options);
+    }
+  }
+
+  /* FUNCAO AUXILIAR NA BUSCA DO INDEX DO SELECT */
+  function finder(data, country) {
+    for (i = 0; i < data.length; i += 1) {
+      if (data[i].country === country) {
+        return i;
+      }
+    }
+  }
+
+  /* CONFIGURAÇÕES DO OPTION SELECIONADO */
+  const customOneName = document.getElementById("customOneName");
+  const customOneOpt = document.getElementById("countries");
+  let index = 0;
+  customOneOpt.addEventListener("change", function () {
+    index = finder(data, customOneOpt.value);
+    if (customOneOpt.value != "default") {
+      tableConstructor(data, customOneOpt.value);
+      let compare = dataNumberValidity();
+    if(compare.includes(customOneOpt.value) === false ) {  
+      graphicUpdate();
+    } else {
+      alert('Anderson, precisa personalizar os nomes que estão diferente, com')
+    }
+    }
+    customOneOpt.value = "default";
+  });
+
+  let posicao = 1;
+  const headerElement = document.getElementsByTagName("header")[0];
+  const thKeys = Object.keys(data[0]);
+  function tableConstructor(data, country) {
+    let div = document.getElementById("alertMessage");
+    for (i = 0; i < tableDelete.length; i += 1) {
+      if (tableDelete[i].classList.item(1) === country) {
+        div.style.display = "block";
+        div.innerHTML = `Ops, ${country} is being shown in the table, please choose another country for comparison`;
+        headerElement.appendChild(div);
+        setTimeout(function () {
+          div.style.display = "none";
+        }, 7000);
+        return;
+      }
+    }
+
+    const rowFather = document.getElementsByTagName("tbody")[0];
+    let newRow = document.createElement("tr");
+    newRow.classList.add(posicao);
+    rowFather.appendChild(newRow);
+
+    let newTH = document.createElement("th");
+    newTH.innerHTML = posicao;
+    newTH.scope = "row";
+    newTH.classList.add("numero", posicao);
+
+    let tableIndex = finder(data, country);
+
+    // SOLUCIONANDO O PROBLEMA DE NOMES COM ESPAÇO ***************************
+    let name = data[tableIndex][thKeys[0]].split(' ')
+    if( name.length > 1) {
+    name = name.join("-")
+    }
+    // FIM ********************************************************************
+    newRow.classList.add(name);
+    let totalCase = data[tableIndex][thKeys[1]];
+    let todayCases = data[tableIndex][thKeys[2]];
+    let totalDeaths = data[tableIndex][thKeys[3]];
+    let todayDeaths = data[tableIndex][thKeys[4]];
+    let comparsionCase = (todayCases / totalCase) * 100;
+    let comparsionDeaths = (todayDeaths / totalDeaths) * 100;
+    for (i = 0; i < 12; i += 1) {
+      let newData = document.createElement("td");
+      let bgColor = randomColor();
+      newRow.appendChild(newData);
+      if (data[tableIndex][thKeys[i]] != null) {
+        newData.innerHTML = data[tableIndex][thKeys[i]].toLocaleString("pt-BR");
+        newData.className = "text-center";
+        newData.classList.add(posicao);
+      }
+      if (newData.innerHTML == '') {
+          newData.innerHTML = 'N/D';
+        }
+      bgColorArray.push(bgColor);
+      if (thKeys[i] === "todayCases") {
+        if (data[tableIndex][thKeys[i]] != 0) {
+          newData.innerHTML = `${data[tableIndex][thKeys[i]].toLocaleString(
+            "pt-BR"
+          )} <span class="text-info" style="font-size: 12px">(${comparsionCase.toFixed(
+            2
+          )}%)<i class="fas fa-level-up-alt"></i></span> `;
+        }
+      }
+      if (thKeys[i] === "todayDeaths") {
+        if (data[tableIndex][thKeys[i]] != 0) {
+          newData.innerHTML = `${data[tableIndex][thKeys[i]].toLocaleString(
+            "pt-BR"
+          )} <span class="text-danger" style="font-size: 12px">(${comparsionDeaths.toFixed(
+            2
+          )}%)<i class="fas fa-level-up-alt"></i></span> `;
+        }
+      }
+    }
+
+    posicao += 1;
+  }
+
+  function randomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    return "rgba(" + r + ", " + g + ", " + b + ", " + "0.7" + ")";
+  }
+
+  let deathInfo = [];
+  let bgColorArray = [];
+
+  tableConstructor(data, "World");
+  tableConstructor(data, "USA");
+  tableConstructor(data, "Brazil");
+  tableConstructor(data, "Spain");
+  tableConstructor(data, "Italy");
+  tableConstructor(data, "France");
+  optionConstruction(data);
+}
+const deletedRow = document.getElementsByTagName("tbody")[0];
+let deletePosition = 1000;
+deletedRow.addEventListener("click", selectToDelete);
+let tableDelete = document.getElementsByTagName("tr");
+function selectToDelete(event) {
+  let target = event.target;
+  let compare = target.classList.item(1);
+
+  for (i = 0; i < tableDelete.length; i += 1) {
+    if (tableDelete[i].classList.contains(compare)) {
+      if (tableDelete[i].classList.contains("bg-warning")) {
+        tableDelete[i].classList.remove("bg-warning");
+      } else {
+        for (let j = 0; j < tableDelete.length; j += 1) {
+          tableDelete[j].classList.remove("bg-warning");
+        }
+        tableDelete[i].classList.add("bg-warning");
+        deletePosition = i;
+      }
+    }
+  }
+}
+
+let deleteButton = document.getElementsByClassName("remove")[0];
+deleteButton.addEventListener("click", function () {
+  deleteAll();
+  function deleteAll() {
+    for (i = 0; i < tableDelete.length; i += 1) {
+      if (tableDelete[i].classList.contains("bg-warning")) {
+        deletedRow.removeChild(tableDelete[i]);
+        deleteAll();
+      }
+    }
+  }
+});
+
+let numberCorrection = document.getElementsByClassName("numero");
